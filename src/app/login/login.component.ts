60050c3f33a93b1b5c95027310ba8a4345eb1a81
DESKTOP-I0S9ABL\davel <dragan.jovanovic.gale@gmail.com>
2024-10-28 23:56:05 +0000

    Mssf-7:Fixed up login page to work with blur added scss

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth/auth.service';
@@ -12,17 +12,47 @@ import { HttpResponse } from '@angular/common/http';
   imports: [CommonModule, ReactiveFormsModule],
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  errorMessage: string | null = null; 
+  errorMessage: string | null = null;
+  passwordVisible = false;
+  capsLockOn = false; 
+  blurredEmail = false;
 
-  constructor(private fb: FormBuilder,private authService:AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
- 
+
+  ngOnInit(): void {
+    this.checkCapsLockStatusOnInit();
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  // Check Caps Lock on initialization
+  checkCapsLockStatusOnInit(): void {
+    document.addEventListener('keydown', (event) => {
+      this.capsLockOn = event.getModifierState && event.getModifierState('CapsLock');
+    });
+  }
+
+  // Check Caps Lock during typing events
+  checkCapsLock(event: KeyboardEvent): void {
+    this.capsLockOn = event.getModifierState && event.getModifierState('CapsLock');
+  }
+
+  onEmailBlur() {
+    this.blurredEmail = true;
+  }
+
+  onEmailFocus(): void {
+    this.blurredEmail = false;
+  }
 
   onSubmit(): void {
     if (this.loginForm.valid) {
@@ -30,11 +60,11 @@ export class LoginComponent {
       const password = this.loginForm.get('password')!.value;
 
       this.authService.login(username, password).subscribe(
-        (res: HttpResponse<boolean>) => {},
+        (res: HttpResponse<boolean>) => { },
         (error: any) => { 
           this.errorMessage = 'Login failed. Please check your credentials.';
         }
-      ); 
+      );
     }
   }
 }
