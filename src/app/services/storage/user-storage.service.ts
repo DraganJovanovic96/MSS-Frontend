c372d1878b429c27cb4e206f4605d70a5ad5902c
DESKTOP-I0S9ABL\davel <dragan.jovanovic.gale@gmail.com>
2024-11-13 21:18:37 +0000

    mssf-35: Delete console logs

--- src/app/services/storage/user-storage.service.ts
+++ src/app/services/storage/user-storage.service.ts
@@ -9,16 +9,12 @@ export class UserStorageService {
   private USER = 'mss-user';
 
   saveToken(token: string): void {
-    console.log("Saving access token:", token);
     localStorage.setItem(this.tokenKey, token);
-    console.log("Access token saved successfully.");
   }
 
   public saveUser(user: Object): void {
-    console.log("Saving user data:", user);
     window.localStorage.removeItem(this.USER);
     window.localStorage.setItem(this.USER, JSON.stringify(user));
-    console.log("User data saved successfully.");
   }
 
   public getUser(): string | null {
@@ -32,9 +28,7 @@ export class UserStorageService {
   }
 
   saveRefreshToken(token: string): void {
-    console.log("Saving refresh token:", token);
     localStorage.setItem(this.refreshTokenKey, token);
-    console.log("Refresh token saved successfully.");
   }
 
   getRefreshToken(): string | null {
@@ -43,9 +37,7 @@ export class UserStorageService {
   }
 
   clearTokens(): void {
-    console.log("Clearing tokens");
     localStorage.removeItem(this.tokenKey);
     localStorage.removeItem(this.refreshTokenKey);
-    console.log("Tokens cleared successfully.");
   }
 }
